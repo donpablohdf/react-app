2735911f76c9e6f68f7f2a74e6799f3a5ff36b14
Pablo Alegre <pabloalegrejuan@gmail.com>
2023-01-19 23:39:29 +0000

    entrar con google

--- src/front/js/component/App.js
+++ src/front/js/component/App.js
@@ -1,29 +1,48 @@
 import React, { useContext, useState, useEffect } from "react";
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
 import { Context } from "../../store/appContext";
 import '../../css/App.css'
 function App() {
+  const clientId = '947353548654-r37sov1ij6j67tlucjat853tclofhgig.apps.googleusercontent.com';
+
+
+  const onSuccess = (res) => {
+    console.log('success:', res);
+  };
+  const onFailure = (err) => {
+    console.log('failed:', err);
+  };
   //console.clear()
   const { actions, store } = useContext(Context);
 
   const [usuario, setUsuario] = useState(store.data);
 
   const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    /* global google */
+    if (window.google) {
+      google.accounts.id.initialize({
+        client_id:'947353548654-r37sov1ij6j67tlucjat853tclofhgig.apps.googleusercontent.com',
+        callback: onSuccess,
+      });
 
+      google.accounts.id.renderButton(document.getElementById("loginDiv"), {
+        // type: "standard",
+        theme: "filled_black",
+        // size: "small",
+        text: "signin_with",
+        shape: "pill",
+      });
+
+      // google.accounts.id.prompt()
+    }
+  }, [onSuccess]);
   useEffect(() => {
 
-    /*actions.dataFromAPI("/api/usuario/1").then((datos) => {
-      console.log(datos)
-      setUsuario(datos);
-      setIsLoading(false);
-    });*/
-    const promesa = () => {
-      return new Promise((resolve, reject) => {
-        resolve(actions.dataFromAPI("/api/usuario/1"));
-      });
-    };
-    promesa().then((datos) => {
+    actions.dataFromAPI("/api/usuario/1").then((datos) => {
       setUsuario(store.data);
-      store.data=null
+      store.data = null
       setIsLoading(false);
     });
 
@@ -37,8 +56,28 @@ function App() {
     );
   }
   return (
+    
     <div className="container bg-info">
-
+      <div id="loginDiv"></div>
+      <script src="https://accounts.google.com/gsi/client" async defer></script>
+      <div id="g_id_onload"
+        data-client_id={clientId}
+        data-context="signin"
+        data-ux_mode="popup"
+        data-callback="onSuccess"
+        data-auto_prompt="false">
+      </div>
+      <button>
+      <div className="g_id_signin"
+        data-type="standard"
+        data-shape="pill"
+        data-theme="filled_blue"
+        data-text="continue_with"
+        data-size="large"
+        data-locale="es"
+        data-logo_alignment="left"> 
+      </div>
+      </button>
 
       <h1>
         {usuario.nombre}
